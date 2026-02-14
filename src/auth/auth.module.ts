70192fffa9d2e5bc76f59a1bc7b465522b8ef85f
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-14 11:48:53 +0200

    feat: implement AuthGuard in UsersController and update module imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService],
-    imports: [UsersModule],
+    providers: [AuthService, AuthGuard],
+    imports: [forwardRef(() => UsersModule)],
+    exports: [AuthGuard],
 })
 export class AuthModule {}
