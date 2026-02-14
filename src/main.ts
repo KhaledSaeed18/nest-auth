bf97edc689e2e80b001cc2fcef738f98a578bc97
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-14 12:32:17 +0200

    feat: add LoggingInterceptor for request logging

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './interceptors/logger.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
             forbidNonWhitelisted: true,
         }),
     );
+    app.useGlobalInterceptors(new LoggingInterceptor());
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
