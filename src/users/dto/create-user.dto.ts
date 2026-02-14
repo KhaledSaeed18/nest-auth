982cbb54f209675d1d07806458d6a0fa6d6c746e
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-14 09:52:29 +0200

    feat: implement user DTOs with validation and update user service

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import {
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    MinLength,
+} from 'class-validator';
+import { UserRole } from 'src/types/role.enum';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+    @IsOptional()
+    @IsEnum(UserRole)
+    role: UserRole;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+    IsEmail,
+    IsEnum,
+    IsOptional,
+    IsString,
+    MinLength,
+} from 'class-validator';
+import { UserRole } from 'src/types/role.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @IsOptional()
+    @IsEmail()
+    email?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(6)
+    password?: string;
+
+    @IsOptional()
+    @IsEnum(UserRole)
+    role?: UserRole;
+}
