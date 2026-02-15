d0d458ad518459c66102b759918a88368739298c
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-15 12:19:59 +0200

    feat: add helmet for security middleware and update package dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './interceptors/logger.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -23,6 +24,9 @@ async function bootstrap() {
         .build();
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, documentFactory);
+
+    // Security middleware, using Helmet to set various HTTP headers for security
+    app.use(helmet());
 
     await app.listen(process.env.PORT ?? 3000);
 }
