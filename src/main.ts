cee770bb9a4bc9dd6cfc9efc7c690e3aa74301ca
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-15 10:46:29 +0200

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './interceptors/logger.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
         }),
     );
     app.useGlobalInterceptors(new LoggingInterceptor());
+
+    // Swagger configuration (API documentation)
+    const config = new DocumentBuilder()
+        .setTitle('NestJS Auth API')
+        .setDescription('The NestJS Auth API description')
+        .setVersion('1.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
